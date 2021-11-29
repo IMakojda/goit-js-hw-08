18f5c2b413baaebe4e62b6578c1831710708d87c
Igor Makojda <imakojda@gmail.com>
2021-11-29 21:43:29 +0200

    done task 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,32 @@
 
+import  throttle  from 'lodash.throttle';
+import Player from '@vimeo/player';
+
+const player = new Player(document.getElementById('vimeo-player'));
+
+
+
+player.on('timeupdate', throttle(onTimeupdate, 1000));
+
+function onTimeupdate(){
+    player.getCurrentTime().then( function(seconds) {
+           // seconds = the current playback position
+           localStorage.setItem('videoplayer-current-time',seconds);
+    });
+}
+
+const timeEnd=localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(timeEnd).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from "lodash.throttle";
 
+const form=document.querySelector('.feedback-form');
+
+const keyStorage='feedback-form-state';
+
+const formObject={};
+
+const refs={
+    form:document.querySelector('.feedback-form'),
+    textarea:document.querySelector('.feedback-form textarea')
+}
+
+refs.form.addEventListener('submit',onFormSubmit);
+
+refs.form.addEventListener('input',throttle( onTextInput, 500));
+
+savedMessageLocal()
+
+function savedMessageLocal(){
+   
+    const savedMessage=localStorage.getItem(keyStorage);
+    if (savedMessage){
+          const formInput=JSON.parse(localStorage.getItem(keyStorage));
+            form.email.value=formInput.email;
+            form.message.value=formInput.message;
+    }   
+}
+
+function onFormSubmit(evt) {
+    console.log(JSON.parse(localStorage.getItem(keyStorage)));
+    evt.preventDefault();
+    evt.currentTarget.reset();
+    localStorage.removeItem(keyStorage)
+    
+}
+
+function onTextInput(evt){
+    formObject[evt.target.name]=evt.target.value;
+    console.log(formObject);
+    localStorage.setItem(keyStorage,JSON.stringify(formObject))
+}
